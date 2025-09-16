6060868593c939d22f0785a765c263337bce0e4e
kelocoes <kevin.rodriguez109@gmail.com>
2025-09-16 16:06:30 -0500

    Init project

--- eslint.config.js
+++ eslint.config.js
@@ -1,62 +1,23 @@
-import { defineConfig } from "eslint/config";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintRecommended from "@eslint/js";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-const compat = new FlatCompat({
-    baseDirectory: import.meta.dirname || process.cwd(),
-    recommendedConfig: eslintRecommended.configs.recommended
-});
-
-export default defineConfig([
-    ...compat.config({
-        extends: [
-            "plugin:react/recommended",
-            "plugin:react-hooks/recommended",
-            "plugin:@typescript-eslint/recommended",
-            "eslint:recommended",
-            "plugin:import/recommended",
-            "plugin:import/typescript",
-        ],
-        env: {
-            browser: true,
-            node: true,
-        },
-        rules: {
-            "react/prop-types": 0,
-            "react/react-in-jsx-scope": "off",
-            "max-len": ["error", { code: 250 }],
-            "quotes": ["error", "double"],
-            "no-unused-vars": ["error", {
-                vars: "all",
-                args: "all",
-                argsIgnorePattern: "^_"
-            }],
-            "import/order": [
-                "error",
-                {
-                    "groups": [
-                        "builtin",
-                        "external",
-                        "internal",
-                        "parent",
-                        "sibling",
-                        "index"
-                    ],
-                    "newlines-between": "always"
-                }
-            ],
-            "indent": ["error", 4],
-            "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
-            "block-spacing": "error",
-            "semi": ["error", "always"],
-            "camelcase": "off",
-            "no-console": ["error", { allow: ["info", "warn", "error"] }]
-        },
-        ignorePatterns: ["dist", "build", "vite.config.ts", "eslint.config.js"],
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-    }),
-]);
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
