c37e468f6d3b8b0f6d1ecb1a171573ce2d5c7b14
Kelocoes <kevin.rodriguez109@gmail.com>
2025-11-11 12:41:23 -0500

    Fix?

--- src/services/supabase/authService.ts
+++ src/services/supabase/authService.ts
@@ -8,6 +8,7 @@ const authService = {
                 password,
                 options: {
                     data: metadata, // Información adicional del usuario si desean agregarla
+                    emailRedirectTo: "https://demo-curso.onrender.com/",
                 },
             });
 
@@ -126,7 +127,7 @@ const authService = {
     forgotPassword: async (email: string) => {
         try {
             const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-                redirectTo: window.location.origin +"/update-password",
+                redirectTo: window.location.origin + "/update-password",
             });
 
             if (error) {
